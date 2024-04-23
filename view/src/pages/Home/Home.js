ed1d01d6daf9d0fb66aa5659d6b12f6e984a3b19
GO-Shoderu <go.shoderu@gmail.com>
2024-04-23 19:37:54 +0200

    Adding Navigation to the Home page

---
+++ view/src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import Nav from "../constant/Nav";
+import Hero from "./Hero";
+
+const Home = () => {
+  return (
+    <div>
+      <Nav />
+      <Hero />;
+    </div>
+  );
+};
+
+export default Home;
