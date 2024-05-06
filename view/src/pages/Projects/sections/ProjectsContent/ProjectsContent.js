565f46aacb99fde18d9213210e5889c1be3d7e5f
GO-Shoderu <go.shoderu@gmail.com>
2024-05-06 17:32:48 +0200

    Adding the real data to the projects page

--- view/src/pages/Projects/sections/ProjectsContent/ProjectsContent.js
+++ view/src/pages/Projects/sections/ProjectsContent/ProjectsContent.js
@@ -1,8 +1,8 @@
-import data from "../../../../assets/static_data/services.json";
+import data from "../../../../assets/static_data/projects.json";
 
-import img_1 from "../../../../assets/image/scott-graham-5fNmWej4tAA-unsplash.jpg";
-import img_2 from "../../../../assets/image/cytonn-photography-GJao3ZTX9gU-unsplash.jpg";
-import img_3 from "../../../../assets/image/pexels-tima-miroshnichenko-6693656.jpg";
+import img_1 from "../../../../assets/image/Picture1.jpg";
+import img_2 from "../../../../assets/image/Picture3.jpg";
+import img_3 from "../../../../assets/image/Picture5.png";
 
 import img_4 from "../../../../assets/image/pexels-yan-krukau-7691694.jpg";
 import img_5 from "../../../../assets/image/pexels-ketut-subiyanto-4623463.jpg";
@@ -27,80 +27,90 @@ const ProjectsContent = () => {
   return (
     <div className="container py-3 mt-4 bg-light">
       <div className="row justify-content-center">
-        {data.services.map((service, iter) => (
+        <h2 className="text-primary text-center">COMPLETED PROJECTS</h2>
+        {data.projects.map((project, iter) => (
           <div
             className="col-sm-12 col-md-6 col-lg-4 mt-4"
             data-aos="fade-up"
             data-aos-easing="linear"
             data-aos-duration={getRandomValue().toString()}
-            key={service.name}
+            key={project.name}
           >
             <div className="card  h-100 d-flex flex-column bg-body-secondary">
               {iter === 0 && (
                 <img
                   src={img_1}
                   className="card-img-top"
-                  alt={service.name + " image"}
+                  alt={project.name + " image"}
                 />
               )}
               {iter === 1 && (
                 <img
                   src={img_2}
                   className="card-img-top"
-                  alt={service.name + " image"}
+                  alt={project.name + " image"}
                 />
               )}
               {iter === 2 && (
                 <img
                   src={img_3}
                   className="card-img-top"
-                  alt={service.name + " image"}
+                  alt={project.name + " image"}
                 />
               )}
               {iter === 3 && (
                 <img
                   src={img_4}
                   className="card-img-top"
-                  alt={service.name + " image"}
+                  alt={project.name + " image"}
                 />
               )}
               {iter === 4 && (
                 <img
                   src={img_5}
                   className="card-img-top"
-                  alt={service.name + " image"}
+                  alt={project.name + " image"}
                 />
               )}
               {iter === 5 && (
                 <img
                   src={img_6}
                   className="card-img-top"
-                  alt={service.name + " image"}
+                  alt={project.name + " image"}
                 />
               )}
               {iter === 6 && (
                 <img
                   src={img_7}
                   className="card-img-top"
-                  alt={service.name + " image"}
+                  alt={project.name + " image"}
                 />
               )}
               {iter === 7 && (
                 <img
                   src={img_8}
                   className="card-img-top"
-                  alt={service.name + " image"}
+                  alt={project.name + " image"}
                 />
               )}
               <div className="card-body d-flex flex-column">
                 <h6 className="card-title text-primary text-uppercase">
-                  {service.name}
+                  {project.name}
                 </h6>
-                <ul className="card-text flex-grow-1">
-                  {service.description.map((item, index) => (
-                    <li key={index}>{item}</li>
-                  ))}
-                </ul>
+                <p className="card-text flex-grow-1">{project.goal}</p>
+
+                <h6 className="card-title text-primary text-uppercase">
+                  Contracted to:
+                </h6>
+                <p className="card-text flex-grow-1">
+                  <i>
+                    Okumbe Consulting Quantity Serveyors <br /> Trinity
+                    Consulting Quantity Surveyours & Associate
+                  </i>
+                </p>
+                <p className="card-text flex-grow-1">
+                  <b>{project.price}</b>
+                </p>
                 {/* <div className="text-center text-sm-start">
                   <a href="./" className="btn btn-primary ">
                     Read More
