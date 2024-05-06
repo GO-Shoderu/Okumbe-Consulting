20dc26b02be32eb395fe6352942ca752a3e9f4a3
GO-Shoderu <go.shoderu@gmail.com>
2024-05-06 22:27:20 +0200

    Dealth with the inconsistencies in the home page with regards to the featured project displays

--- view/src/pages/Home/sections/Project/Project.js
+++ view/src/pages/Home/sections/Project/Project.js
@@ -17,8 +17,6 @@ const Project = () => {
 
   const getRandomValue = () => (Math.random() < 0.5 ? 500 : 800);
 
-  const numOfProjects = 3;
-
   return (
     <div className="container py-3 mt-4 bg-light">
       <h2
@@ -30,53 +28,81 @@ const Project = () => {
         FEATURED PROJECTS
       </h2>
       <div className="row justify-content-center">
-        {data.completed_projects
-          .slice(0, numOfProjects)
-          .map((project, iter) => (
-            <div
-              className="col-sm-12 col-md-6 col-lg-4 mt-4"
-              data-aos="fade-up"
-              data-aos-easing="linear"
-              data-aos-duration={getRandomValue().toString()}
-              key={project.name}
-            >
-              <div className="card  h-100 d-flex flex-column bg-body-secondary">
-                {iter === 0 && (
-                  <img
-                    src={img_1}
-                    className="card-img-top"
-                    alt={project.name + " image"}
-                  />
-                )}
-                {iter === 1 && (
-                  <img
-                    src={img_2}
-                    className="card-img-top"
-                    alt={project.name + " image"}
-                  />
-                )}
-                {iter === 2 && (
-                  <img
-                    src={img_3}
-                    className="card-img-top"
-                    alt={project.name + " image"}
-                  />
-                )}
-                <div className="card-body d-flex flex-column">
-                  <h6 className="card-title text-primary text-uppercase">
-                    {project.name}
-                  </h6>
-                  <p className="card-text flex-grow-1">{project.goal}</p>
-                  <p className="card-text flex-grow-1">{project.price}</p>
-                  {/* <div className="text-center text-sm-start">
-                  <a href="./" className="btn btn-primary ">
-                    Read More
-                  </a>
-                </div> */}
-                </div>
-              </div>
+        <div
+          className="col-sm-12 col-md-6 col-lg-4 mt-4"
+          data-aos="fade-up"
+          data-aos-easing="linear"
+          data-aos-duration={getRandomValue().toString()}
+        >
+          <div className="card  h-100 d-flex flex-column bg-body-secondary">
+            <img
+              src={img_1}
+              className="card-img-top"
+              alt={data.completed_projects[0].name + " image"}
+            />
+            <div className="card-body d-flex flex-column">
+              <h6 className="card-title text-primary text-uppercase">
+                {data.completed_projects[0].name}
+              </h6>
+              <p className="card-text flex-grow-1">
+                {data.completed_projects[0].goal}
+              </p>
+              <p className="card-text flex-grow-1">
+                <b>{data.completed_projects[0].price}</b>
+              </p>
             </div>
-          ))}
+          </div>
+        </div>
+        <div
+          className="col-sm-12 col-md-6 col-lg-4 mt-4"
+          data-aos="fade-up"
+          data-aos-easing="linear"
+          data-aos-duration={getRandomValue().toString()}
+        >
+          <div className="card  h-100 d-flex flex-column bg-body-secondary">
+            <img
+              src={img_3}
+              className="card-img-top"
+              alt={data.ongoing_projects[2].name + " image"}
+            />
+            <div className="card-body d-flex flex-column">
+              <h6 className="card-title text-primary text-uppercase">
+                {data.ongoing_projects[2].name}
+              </h6>
+              <p className="card-text flex-grow-1">
+                {data.ongoing_projects[2].goal}
+              </p>
+              <p className="card-text flex-grow-1">
+                <b>{data.ongoing_projects[2].price}</b>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div
+          className="col-sm-12 col-md-6 col-lg-4 mt-4"
+          data-aos="fade-up"
+          data-aos-easing="linear"
+          data-aos-duration={getRandomValue().toString()}
+        >
+          <div className="card  h-100 d-flex flex-column bg-body-secondary">
+            <img
+              src={img_2}
+              className="card-img-top"
+              alt={data.ongoing_projects[0].name + " image"}
+            />
+            <div className="card-body d-flex flex-column">
+              <h6 className="card-title text-primary text-uppercase">
+                {data.ongoing_projects[0].name}
+              </h6>
+              <p className="card-text flex-grow-1">
+                {data.ongoing_projects[0].goal}
+              </p>
+              <p className="card-text flex-grow-1">
+                <b>{data.ongoing_projects[0].price}</b>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
       <br />
       <div
