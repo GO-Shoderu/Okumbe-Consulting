f045614b9f6d2b5ae54f8e2771c3e7cc07030121
GO-Shoderu <go.shoderu@gmail.com>
2024-05-09 06:48:20 +0200

    Making some ecard additions and some styling

--- view/src/pages/ECard/Ecard.js
+++ view/src/pages/ECard/Ecard.js
@@ -1,11 +1,31 @@
 import React from "react";
 import eCard from "../../assets/eCard/eCard.jpg";
+import eCardPdf from "../../assets/eCard/eCard.pdf";
 import "./Ecard.css";
 
 const Ecard = () => {
   return (
     <div className="container">
-      <img src={eCard} className="ecard" alt="Electronic Business Card" />
+      <div className="row justify-content-center">
+        <div className="col-12 text-center mb-0 pb-0">
+          <img src={eCard} className="ecard" alt="Electronic Business Card" />
+        </div>
+        <div className="col-12 text-center mt-0 pt-0">
+          {" "}
+          {/* Adjust mt-3 based on your desired vertical spacing */}
+          <div className="btn-group">
+            <a
+              href={eCardPdf}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-primary active"
+              aria-current="page"
+            >
+              Download a copy
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
